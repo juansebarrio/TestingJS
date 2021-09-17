f3b2a6a1347dbd8af3b4df3a6f7aa3f9873defd0
juansebarrio <contacto@juansebarrio.com>
2021-09-17 09:10:19 -0300

    Primer Commit

---
+++ pruebas.js
@@ -0,0 +1,29 @@
+// prueba comments
+
+/*
+multi linea
+*/
+
+// VSC shortcut command+/  -  Multilinea  Option + Shift + A
+
+// console.log("hola desde el archivo js");
+
+let variableLET = "soy una variable";
+const PI = 3.14;
+
+// las variables let se pueden modificar mas adelante
+
+variableLET = "123";
+
+console.log(variableLET);
+
+// suma
+let numerouno = 1;
+let numerodos = 2;
+let numerotres = 3;
+let suma = numerouno + numerodos + numerotres;
+alert(suma);
+
+// promptEdad
+let userAge = prompt("Ingresa tu edad");
+console.log(userAge);
