96064f3b0ff960a682a23c6a649cf05e8f5fee43
juansebarrio <contacto@juansebarrio.com>
2021-09-27 20:27:14 -0300

    Clase 3

--- pruebas.js
+++ pruebas.js
@@ -55,6 +55,7 @@ if (condicion) {
 let variableTrue = true;
 let variableFalse = false; */
 
+/*  
 // Condicional mayor de edad
 
 let edad = prompt("Ingrese su edad");
@@ -77,4 +78,185 @@ if (user == _user && pass == _pass) {
   alert("Logueado Correctamente");
 } else {
   alert("Datos Incorrectos");
+}
+*/
+
+/* ciclo for
+// i++ == i + i+1;
+// inicio; fin (condicion de fin); paso
+for (let i = 0; i < 10; i++) {
+  // console.log("Hola Coder!");
+  console.log("Iteracion nro: " + i);
+}
+*/
+
+// Tabla del 7
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log("tabla del 7");
+//   // console.log("7x" + i + "= " + 7*i);
+//   console.log(`7x${i} = ${7 * i}`);
+// }
+
+/* Ciclo WHILE */
+// let i = 0;
+
+// let menu = Number(prompt(
+// 	` Ingresa una de las siguientes opciones
+
+// 1 - Sumar
+// 2 - Restar
+// 3 - Multiplicar
+// 4 - Dividir
+
+// 0 - Finalizar programa
+
+// `));
+// // menu = 0;
+// while (menu != 0) {
+
+// 	switch (menu) {
+// 		case 1:
+// 			let x = Number(prompt("Ingrese el primer nro a sumar"));
+// 			let y = Number(prompt("Ingrese el segundo nro a sumar"));
+// 			alert(`El resultado de ${x} + ${y} = ${x + y}`);
+// 			menu = Number(prompt(
+// 				` Desea realizar otra operacion?:
+
+// 			1 - Sumar
+// 			2 - Restar
+// 			3 - Multiplicar
+// 			4 - Dividir
+
+// 			0 - Finalizar programa
+
+// 			`));
+// 			break;
+// 		case 2:
+// 			alert("RESTAR");
+// 			menu = Number(prompt(
+// 				` Desea realizar otra operacion?:
+
+// 			1 - Sumar
+// 			2 - Restar
+// 			3 - Multiplicar
+// 			4 - Dividir
+
+// 			0 - Finalizar programa
+
+// 			`));
+// 			break;
+// 		case 3:
+// 			alert("MULTIPLICAR");
+// 			menu = Number(prompt(
+// 				` Desea realizar otra operacion?:
+
+// 			1 - Sumar
+// 			2 - Restar
+// 			3 - Multiplicar
+// 			4 - Dividir
+
+// 			0 - Finalizar programa
+
+// 			`));
+// 			break;
+// 		case 4:
+// 			alert("DIVIDIR");
+// 			menu = Number(prompt(
+// 				` Desea realizar otra operacion?:
+
+// 			1 - Sumar
+// 			2 - Restar
+// 			3 - Multiplicar
+// 			4 - Dividir
+
+// 			0 - Finalizar programa
+
+// 			`));
+// 			break;
+// 		default:
+// 			menu = Number(prompt(
+// 				` La opción ingresada es incorrecta:
+
+// 			1 - Sumar
+// 			2 - Restar
+// 			3 - Multiplicar
+// 			4 - Dividir
+
+// 			0 - Finalizar programa
+
+// 			`));
+// 			break;
+// 	}
+// }
+// alert("Programa finalizado");
+
+/* Ciclo DO-WHILE */
+
+/* 
+let menu;
+do {
+	let x, y;
+	menu = Number(prompt(
+		` Ingresa una de las siguientes opciones
+	
+	1 - Sumar
+	2 - Restar
+	3 - Multiplicar
+	4 - Dividir
+	
+	0 - Finalizar programa
+	
+	`));
+	switch (menu) {
+		case 1:
+			x = Number(prompt("Ingrese el primer nro a sumar"));
+			y = Number(prompt("Ingrese el segundo nro a sumar"));
+			alert(`El resultado de ${x} + ${y} = ${x + y}`);
+
+			break;
+		case 2:
+			x = Number(prompt("Ingrese el primer nro a restar"));
+			y = Number(prompt("Ingrese el segundo nro a restar"));
+			alert(`El resultado de ${x} - ${y} = ${x - y}`);
+
+			break;
+		case 3:
+			alert("MULTIPLICAR");
+
+			break;
+		case 4:
+			alert("DIVIDIR");
+
+			break;
+		default:
+			break;
+	}
+} while (menu != 0)
+alert("Programa finalizado");
+ */
+
+/* Ejemplo Estructura DO WHILE */
+const ganador = Math.floor(Math.random() * 100);
+console.log(ganador);
+let fallas = 0;
+let numero;
+do {
+  do {
+    numero = Number(prompt("Ingrese un valor entre 1 y 100"));
+  } while (numero < 1 || numero > 100);
+
+  if (numero == ganador) {
+    alert("Ganaste!");
+  } else if (numero < ganador) {
+    alert("El numero es mas grande!");
+    fallas++;
+  } else {
+    alert("El numero es mas chico!");
+    fallas++;
+  }
+} while (numero != ganador && fallas < 5);
+
+if (numero != ganador) {
+  alert(`Game Over: Tuviste ${fallas} fallas`);
 }
